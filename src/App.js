d00c547ec09f0aebcbc3a58bd2b1b0f31578d0ac
Tim Martin <timmartin13@gmail.com>
2021-05-29 17:11:22 -0500

    Added the data grid from Material-UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
+import { DataGrid } from '@material-ui/data-grid';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const getData = async () => {
+    // The only proxy I could find that would work from this website
+    // https://nordicapis.com/10-free-to-use-cors-proxies/
+    const proxy = "https://thingproxy.freeboard.io/fetch/";
+    const response = await fetch(proxy + "https://fetch-hiring.s3.amazonaws.com/hiring.json");
+    // Get the data
+    const responseData = await response.json();
+    // console.log({ responseData });
+    const cleanData = await responseData
+      // Filter null or empty names
+      .filter((entry) => entry.name)
+      // Sort by listId and then by id which is the same number as in the name
+      .sort((a, b) => {
+        return a.listId - b.listId || a.id - b.id;
+      });
+    // console.log({ cleanData });
+    // Put clean/sorted data into our state variable
+    setData(cleanData);
+  };
+
+  // Get the data on mount
+  useEffect(() => {
+    getData();
+  }, []);
+
+  // Data Grid essentials here: https://material-ui.com/components/data-grid/getting-started/
+  const columns = [
+    { field: 'listId', headerName: 'ListId', width: 120 },
+    { field: 'name', headerName: 'Name', width: 120 },
+    { field: 'id', headerName: 'Id', width: 120 },
+  ];
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          Data
       </header>
+      <DataGrid rows={ data } columns={ columns } />
     </div>
   );
 }
